5c4d6ed44eb0cf70672e77f846f46cc7e30e27eb
f-kurdan <fkurdan@gmail.com>
2022-06-15 19:19:03 +0300

    Add attributes to model props

--- TestDeliveryApp/Models/Order.cs
+++ TestDeliveryApp/Models/Order.cs
@@ -9,20 +9,30 @@ namespace TestDeliveryApp.Models
         public int ID { get; set; }
         [Required]
         [Display(Name = "Sender City")]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{3,50}$",
+            ErrorMessage = "Title cannot be shorter than 3 or longer than 50 characters.")]
         public string SenderCity { get; set; }
         [Required]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{3,50}$",
+            ErrorMessage = "Title cannot be shorter than 3 or longer than 50 characters.")]
         [Display(Name = "Sender Adress")]
         public string SenderAdress { get; set; }
         [Required]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{3,50}$",
+            ErrorMessage = "Title cannot be shorter than 3 or longer than 50 characters.")]
         [Display(Name = "Recipient City")]
         public string RecipientCity { get; set; }
         [Required]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{3,50}$",
+            ErrorMessage = "Title cannot be shorter than 3 or longer than 50 characters.")]
         [Display(Name = "Recipient Adress")]
         public string RecipientAdress { get; set; }
         [Required]
         [Display(Name = "Cargo Weight")]
         public int CargoWeight { get; set; }
         [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [Display(Name = "Cargo Pick Up Date")]
         public DateTime CargoPickUpDate { get; set; }
     }
